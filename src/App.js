1d6c1716b013b9db6e07f9aaa3e2f507b56205dc
Okera Johnson <okerajohnson@gmail.com>
2023-05-14 17:04:45 -0400

    ai analyze app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Configuration, OpenAIApi } from "openai";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+
+
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "You are a psychotherapist analyze the users writing and give helpful advice based on cognitive based therapy abd evidence based therapy`/n/n I have been having lots of drug dreams lately. I dont know what to do ith my life or wwhy I feel so depressed. I struggle to stay sober and am just feeling so useless. I want to have a better life but keep falling back to my old havits and thought patterns. I feel like everyone else is happy but I am the only failure.",
+      temperature: 0.5,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } catch(error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
 }
 
-export default App;
+
+
+
